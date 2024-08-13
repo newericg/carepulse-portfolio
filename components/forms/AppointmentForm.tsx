037984c0f1a2bba9c68c87e30696c72539de80dd
newericg1~ <eric.gomes0105@gmail.com>
2024-08-13 18:55:17 -0300

    added admin panel

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -105,6 +105,7 @@ export const AppointmentForm = ({
           type,
         };
 
+        console.log(appointmentToUpdate)
         const updatedAppointment = await updateAppointment(appointmentToUpdate);
 
         if (updatedAppointment) {
